968a7b0aa218be068cc13b8fd1f8d6b20a84b70a
NVG-064 <aricandra064@student.esaunggul.ac.id>
2022-12-01 21:52:41 +0700

    Save progress

--- bin/dart_simple_aes256_encr.dart
+++ bin/dart_simple_aes256_encr.dart
@@ -1,5 +1,93 @@
-import 'package:dart_simple_aes256_encr/dart_simple_aes256_encr.dart' as dart_simple_aes256_encr;
+import 'package:dart_simple_aes256_encr/aes.dart' as cryptography;
+import 'package:dart_simple_aes256_encr/person.dart';
+import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_simple_aes256_encr.calculate()}!');
+  List<Person> persons = [];
+  var currentSelect = -1;
+  var currentAccount = -1;
+  var idUser = -1;
+
+  print("\x1B[2J\x1B[0;0H");
+  print('Welcome');
+  sleep(Duration(seconds: 3));
+  print("\x1B[2J\x1B[0;0H");
+
+  for (;;) {
+    print(
+        'Hi, ${(currentAccount >= 0) ? "${persons[currentAccount].getName(currentAccount)}." : "Anonymous. Please select account first"}\nHave a nice day\n');
+    print('============== MAIN MENU ==============\n');
+    print('${(currentSelect == 1) ? "->" : "  "} 1: Send Message');
+    print('${(currentSelect == 2) ? "->" : "  "} 2: Read Message\n');
+    print('${(currentSelect == 3) ? "->" : "  "} 3: Account\n');
+    print('${(currentSelect == 0) ? "->" : "  "} 0: Exit\n');
+    print('Select: ');
+    var input = stdin.readLineSync();
+
+    switch (input) {
+      case '1':
+        currentSelect = 1;
+        break;
+
+      case '2':
+        currentSelect = 2;
+        break;
+
+      case '3':
+        currentSelect = 3;
+        bool state = true;
+
+        while (state == true) {
+          print("\x1B[2J\x1B[0;0H");
+          print('============== ACCOUNT ==============\n');
+          print('1: Show All Account\n');
+          print('2: Add Account');
+          print('3: Select Account\n');
+          print('0: Exit');
+          input = stdin.readLineSync();
+
+          switch (input) {
+            case '1':
+              if (persons.isNotEmpty) {
+                persons[1].getName(0);
+              } else {
+                print("\x1B[2J\x1B[0;0H");
+                print('Please add account first');
+              }
+              sleep(Duration(seconds: 3));
+              break;
+
+            case '2':
+              idUser++;
+              Person person = Person("Budi", idUser);
+              persons.add(person);
+              break;
+
+            // case '3':
+            //   print('Select User ID: ');
+            //   input = stdin.readLineSync();
+            //   currentAccount =
+            //   break;
+
+            case '0':
+              state = false;
+              break;
+
+            default:
+              break;
+          }
+        }
+        break;
+
+      case '0':
+        print("\x1B[2J\x1B[0;0H");
+        print('Thank you');
+        sleep(Duration(seconds: 3));
+        exit(0);
+      default:
+        break;
+    }
+
+    print("\x1B[2J\x1B[0;0H");
+  }
 }
--- lib/person.dart
+++ lib/person.dart
@@ -1,51 +1,34 @@
 class Person {
-  var _name;
-  var _id;
+  var _name = '';
+  var _id = -1;
   var _secretKey;
   var _nonce;
   var _mac;
   List<int> _ciphertext = [];
   var _onlyToUserId;
 
-  Person(
-    name,
-    id,
-    ciphertext, onlyToUserId, 
-  ) {
+  Person(name, id) {
     _name = name;
     _id = id;
-    _ciphertext = ciphertext;
   }
 
-  String get name {
-    return _name;
+  String getName(id) {
+    if (id == _id) {
+      return _name;
+    } else {
+      return '';
+    }
   }
 
   set name(name) {
     _name = name;
   }
 
-  int get id {
+  get id {
     return _id;
   }
 
   set id(id) {
-    _id = (id >= 0) ? id : 0;
-  }
-
-  List get cipherText {
-    return _ciphertext;
-  }
-
-  set cipherText(ciphertext) {
-    _ciphertext = ciphertext;
-  }
-
-  int get onlyToUserId {
-    return _onlyToUserId;
-  }
-
-  set onlyToUserId(onlyToUserId) {
-    _onlyToUserId = (onlyToUserId >= 0) ? onlyToUserId : 0;
+    _id = (id >= 0) ? id : -1;
   }
 }
